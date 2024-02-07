c4c2b423e172cc556556477764cbd29173a35216
Marcel Bialas <marcelbialas@gmail.com>
2024-02-07 14:26:08 +0100

    added filter dropdown

--- components/page-toolbar.tsx
+++ components/page-toolbar.tsx
@@ -1,17 +1,36 @@
 import React from "react";
 
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
 type Props = {};
 
 const PageToolbar = (props: Props) => {
   return (
-    <div>
+    <div className="w-full flex flex-col gap-6 md:flex-row md:place-content-between ">
       <div>
         <input
           type="search"
           name="search"
-          placeholder="Nach Land suchen..."
-          className="py-2 px-4 w-[30vw] shadow-sm rounded placeholder:text-sm bg-input "
+          placeholder="Search for a Country..."
+          className="py-2 px-4 w-full md:w-[35vw] shadow-sm rounded placeholder:text-sm bg-input "
         />
+      </div>
+      <div>
+        <select className="bg-input py-3 px-6 text-sm ">
+          <option value="">Filter by Region</option>
+          <option value="Africa">Africa</option>
+          <option value="America">America</option>
+          <option value="Asia">Asia</option>
+          <option value="Europe">Europe</option>
+          <option value="Oceania">Oceania</option>
+        </select>
       </div>
     </div>
   );
